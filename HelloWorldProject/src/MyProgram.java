9bf73cd3be5ec174a74754c310918461aca11bdf
shaanMaheshwari <ShaanMaheshwari2006@gmail.com>
2025-01-13 16:07:06 +0000

    working 1/13

--- HelloWorldProject/src/MyProgram.java
+++ HelloWorldProject/src/MyProgram.java
@@ -17,15 +17,33 @@ public class MyProgram {
 		int limitTrackA = 100000, limitTrackB = 100000, limitTrackC = 100000;
 	
 		Scanner x = new Scanner(System.in);
+		Queue<String> trainQ = new LinkedList<String>();
+
 		try{
 			File f = new File("HelloWorldProject/src/data.txt");
 			x = new Scanner (f);
 			String name = x.nextLine();
 			System.out.println(name);
+			trainQ.add(name);
 		}
 		catch(Exception e)
 		{
 			System.out.println(e.getMessage());
 		}
+
+		Stack<Train> first = new Stack<>();
+		Stack<Train> track1 = new Stack<>();
+		Stack<Train> trackA = new Stack<>();
+		Stack<Train> trackB = new Stack<>();
+		Stack<Train> trackC = new Stack<>();
+		Stack<Train> trackD = new Stack<>();
+
+		while(!trainQ.toLowerCase.equals("end")){
+			Train car = trainQ.peek();
+			if(car.getName().substring(0, 3).equals("CAR") && car.getMiles()> 700){
+				track1.push(car);
+			}
+		}
+
 	}
 }
--- HelloWorldProject/src/Train.java
+++ HelloWorldProject/src/Train.java
@@ -2,5 +2,45 @@ public class Train {
     private String name, product, origin, destination;
     private int weight, miles;
 
+    public Train(String nombre, String contents, String start, String city, int size, int distance){
+        name = nombre;
+        product = contents;
+        origin = start;
+        destination = origin;
+        weight = size;
+        miles = distance;
+    }
 
+    public Train(String nombre, String city){
+        name = nombre;
+        destination = city;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getProduct(){
+        return product;
+    }
+
+    public String getOrigin(){
+        return origin;
+    }
+
+    public String getDestination(){
+        return destination;
+    }
+
+    public int getWeight(){
+        return weight;
+    }
+
+    public int getMiles(){
+        return miles;
+    }
+
+    public String toString(){
+        return "Name: " + this.name + "\n Product: " + this.product + "\n Origin: " + this.origin + "\n Destination: " + this.destination + "\n Weight: " + this.weight + "\n Miles: " + this.miles;
+    }
 }
